48a695e83c766d4d759ff242e237cd00012414cb
Kungusto <specifikbrateevo@gmail.com>
2025-03-19 16:34:18 +0300

    начало работы над логикой уведомлений пользователя

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -4,7 +4,12 @@ from src.repositories.base import BaseRepository
 from src.models.users import UsersOrm
 from src.schemas.users import User
 
+
 class UsersRepository(BaseRepository)  :
     model = UsersOrm
     schema = User
 
+    async def get_tg_id_by_id(self, id) : 
+        query = select(UsersOrm).filter_by(id=id)
+        user = await self.session.execute(query)
+        return user.tg_id
